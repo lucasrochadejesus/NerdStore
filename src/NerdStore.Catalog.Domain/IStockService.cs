ba7cbc24984bfb5837821b1f08de29b831d7cfae
Lucas Rocha de Jesus <76123986+lucasrochadejesus@users.noreply.github.com>
2023-05-25 22:18:57 -0600

    StockService Domain

---
+++ src/NerdStore.Catalog.Domain/IStockService.cs
@@ -0,0 +1,8 @@
+﻿namespace NerdStore.Catalog.Domain;
+
+public interface IStockService : IDisposable
+{
+    Task<bool> IncreaseStock(Guid productId, int quantity);
+    Task<bool> DecreaseStock(Guid productId, int quantity);
+
+}
---
+++ src/NerdStore.Catalog.Domain/StockService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class StockService : IStockService
+    {
+
+        private readonly IProductRepository _productRepository;
+
+        public StockService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> IncreaseStock(Guid productId, int quantity)
+        {
+            var product = await _productRepository.GetProductById(productId);
+
+            if (product == null) return false;
+             
+            product.IncreaseStock(quantity);
+
+            _productRepository.Update(product);
+            return await _productRepository.UnitOfWork.Commit();
+
+        }
+
+        public async Task<bool> DecreaseStock(Guid productId, int quantity)
+        {
+            var product = await _productRepository.GetProductById(productId);
+
+            if (product == null) return false;
+
+            if (!product.HasStock(quantity)) return false;
+
+            product.DecreaseStock(quantity);
+
+            _productRepository.Update(product);
+            return await _productRepository.UnitOfWork.Commit(); 
+
+        }
+
+        public void Dispose()
+        {
+          _productRepository.Dispose();
+        }
+    }
+}
