8274dbb29c09029b4ba4ea4b53cd7c86a2de5d3b
Lucas Rocha de Jesus <76123986+lucasrochadejesus@users.noreply.github.com>
2023-06-06 21:41:46 -0600

    Added FluentValidation and Create Order Command.

---
+++ src/NerdStore.Sales.Application/Commands/AddOrderItemCommand.cs
@@ -0,0 +1,27 @@
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Sales.Application.Commands
+{
+    public class AddOrderItemCommand : Command
+    {
+
+        public Guid CustomerId { get; private set; }
+
+        public Guid ProductId { get; private set; }
+
+        public string ProductName { get; private set; }
+
+        public int Quantity { get; private set; }
+
+        public decimal UnitPrice { get; private set; }
+
+        public AddOrderItemCommand(Guid customerId, Guid productId, string productName, int quantity, decimal unitPrice)
+        {
+            CustomerId = customerId;
+            ProductId = productId;
+            ProductName = productName;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+    }
+}
